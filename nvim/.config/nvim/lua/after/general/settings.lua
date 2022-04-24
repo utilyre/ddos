ab2f1b968c60b7c3faee387ef250ccc37b251778
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-04-24 20:24:36 +0430

    nvim: optimize indention options

--- nvim/.config/nvim/lua/after/general/settings.lua
+++ nvim/.config/nvim/lua/after/general/settings.lua
@@ -30,12 +30,8 @@ vim.opt.wrap = false
 vim.opt.cursorline = true
 vim.opt.pumheight = 10
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.smartindent = true
-vim.opt.autoindent = true
+vim.opt.shiftwidth = 2
 
 vim.opt.list = true
 vim.opt.listchars:append("trail:·")
