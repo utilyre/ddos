25367a5bc9252235d229f40268f2e38d32af68d9
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-02 21:11:52 +0430

    nvim: mix packer.init and packer.startup

--- nvim/.config/nvim/lua/after/plugins/packer.lua
+++ nvim/.config/nvim/lua/after/plugins/packer.lua
@@ -2,17 +2,18 @@ local packer = require("packer")
 local util = require("packer.util")
 local whichkey = require("which-key")
 
-packer.init({
-  display = {
-    open_fn = function() return util.float({ border = "single" }) end,
+packer.startup({
+  function()
+    for _, plugin in pairs(_G.plugins) do
+      packer.use(plugin)
+    end
+  end,
+  config = {
+    display = {
+      open_fn = function() return util.float({ border = "single" }) end,
+    },
   },
 })
-
-packer.startup(function()
-  for _, plugin in pairs(_G.plugins) do
-    packer.use(plugin)
-  end
-end)
 
 whichkey.register({
   p = {
