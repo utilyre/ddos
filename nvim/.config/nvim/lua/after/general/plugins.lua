a5fc222a423023ff25da46e077b017ff964ee88c
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-02 12:27:15 +0430

    nvim: use packer.init to configure packer

--- nvim/.config/nvim/lua/after/general/plugins.lua
+++ nvim/.config/nvim/lua/after/general/plugins.lua
@@ -6,47 +6,46 @@ end
 local packer = require("packer")
 local util = require("packer.util")
 
-packer.startup({
-  function(use)
-    use("wbthomason/packer.nvim")
-
-    use("famiu/bufdelete.nvim")
-    use("nvim-lua/plenary.nvim")
-    use("kyazdani42/nvim-web-devicons")
-    use("navarasu/onedark.nvim")
-
-    use("neovim/nvim-lspconfig")
-    use("williamboman/nvim-lsp-installer")
-    use("l3mon4d3/luasnip")
-    use("rafamadriz/friendly-snippets")
-
-    use("hrsh7th/nvim-cmp")
-    use("saadparwaiz1/cmp_luasnip")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("hrsh7th/cmp-buffer")
-
-    use("nvim-treesitter/nvim-treesitter")
-    use("p00f/nvim-ts-rainbow")
-    use("windwp/nvim-ts-autotag")
-    use("joosepalviste/nvim-ts-context-commentstring")
-
-    use("nvim-telescope/telescope.nvim")
-    use("kyazdani42/nvim-tree.lua")
-    use("akinsho/toggleterm.nvim")
-    use("folke/which-key.nvim")
-    use("akinsho/bufferline.nvim")
-    use("nvim-lualine/lualine.nvim")
-    use("lewis6991/gitsigns.nvim")
-    use("lukas-reineke/indent-blankline.nvim")
-    use("folke/zen-mode.nvim")
-    use("windwp/nvim-autopairs")
-    use("numtostr/comment.nvim")
-  end,
-  config = {
-    display = {
-      open_fn = function()
-        return util.float({ border = "single" })
-      end,
-    },
+packer.init({
+  display = {
+    open_fn = function()
+      return util.float({ border = "single" })
+    end,
   },
 })
+
+packer.startup(function(use)
+  use("wbthomason/packer.nvim")
+
+  use("famiu/bufdelete.nvim")
+  use("nvim-lua/plenary.nvim")
+  use("kyazdani42/nvim-web-devicons")
+  use("navarasu/onedark.nvim")
+
+  use("neovim/nvim-lspconfig")
+  use("williamboman/nvim-lsp-installer")
+  use("l3mon4d3/luasnip")
+  use("rafamadriz/friendly-snippets")
+
+  use("hrsh7th/nvim-cmp")
+  use("saadparwaiz1/cmp_luasnip")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+
+  use("nvim-treesitter/nvim-treesitter")
+  use("p00f/nvim-ts-rainbow")
+  use("windwp/nvim-ts-autotag")
+  use("joosepalviste/nvim-ts-context-commentstring")
+
+  use("nvim-telescope/telescope.nvim")
+  use("kyazdani42/nvim-tree.lua")
+  use("akinsho/toggleterm.nvim")
+  use("folke/which-key.nvim")
+  use("akinsho/bufferline.nvim")
+  use("nvim-lualine/lualine.nvim")
+  use("lewis6991/gitsigns.nvim")
+  use("lukas-reineke/indent-blankline.nvim")
+  use("folke/zen-mode.nvim")
+  use("windwp/nvim-autopairs")
+  use("numtostr/comment.nvim")
+end)
