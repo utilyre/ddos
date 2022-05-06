e746af4260a94f294f024a2c95365631c9eea72f
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-06 10:36:16 +0430

    nvim: set middlemouse kemap for closing buffer

--- nvim/.config/nvim/lua/after/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/after/plugins/bufferline.lua
@@ -2,6 +2,7 @@ local bufferline = require("bufferline")
 
 bufferline.setup({
   options = {
+    middle_mouse_command = "Bdelete %d",
     show_close_icon = false,
     show_buffer_close_icons = false,
     offsets = { {
