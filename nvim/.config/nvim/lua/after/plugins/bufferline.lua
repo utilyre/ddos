4932f03a65a7ee52f54c0cfe083235183a29ab88
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-07 13:03:15 +0430

    nvim: close buffer by rightclick

--- nvim/.config/nvim/lua/after/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/after/plugins/bufferline.lua
@@ -2,7 +2,7 @@ local bufferline = require("bufferline")
 
 bufferline.setup({
   options = {
-    middle_mouse_command = "Bdelete %d",
+    right_mouse_command = "Bdelete %d",
     show_close_icon = false,
     show_buffer_close_icons = false,
     offsets = { {
