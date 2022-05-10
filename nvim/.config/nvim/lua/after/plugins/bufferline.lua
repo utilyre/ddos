1a570b49188e84d5abf353fb95dac7d026505b98
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-10 13:03:39 +0430

    nvim: better bufferline icons

--- nvim/.config/nvim/lua/after/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/after/plugins/bufferline.lua
@@ -5,6 +5,10 @@ bufferline.setup({
     right_mouse_command = "Bdelete %d",
     show_close_icon = false,
     show_buffer_close_icons = false,
+    indicator_icon = " ",
+    separator_style = { " ", " " },
+    left_trunc_marker = "",
+    right_trunc_marker = "",
     offsets = { {
       text = "Explorer",
       filetype = "NvimTree",
