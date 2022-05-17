268b015f6d9d78eb5f31ae6f73e60e6c7bff9954
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-05-17 11:57:19 +0430

    nvim: remove command mode navigation keymaps

--- nvim/.config/nvim/lua/after/general/keymaps.lua
+++ nvim/.config/nvim/lua/after/general/keymaps.lua
@@ -3,11 +3,6 @@ vim.keymap.set("n", "<s-y>", "y$")
 vim.keymap.set("i", "jk", "<esc>")
 vim.keymap.set("i", "kj", "<esc>")
 
-vim.keymap.set("c", "<c-h>", "<left>")
-vim.keymap.set("c", "<c-j>", "<down>")
-vim.keymap.set("c", "<c-k>", "<up>")
-vim.keymap.set("c", "<c-l>", "<right>")
-
 vim.keymap.set("n", "<c-h>", "<cmd>wincmd h<cr>")
 vim.keymap.set("n", "<c-j>", "<cmd>wincmd j<cr>")
 vim.keymap.set("n", "<c-k>", "<cmd>wincmd k<cr>")
