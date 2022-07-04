354cb1af206c9f93cc45d35eec19c83d641bfa03
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-04 13:28:47 +0430

    nvim: add str_gsub helper to vim global

--- nvim/.config/nvim/lua/after/general/helpers.lua
+++ nvim/.config/nvim/lua/after/general/helpers.lua
@@ -2,6 +2,12 @@ vim.str_isempty = function(str)
   return str == nil or str == ""
 end
 
+vim.str_gsub = function(str, patt, repl, from, to)
+  from = from or 1
+  to = to or str:len()
+  return str:sub(1, from - 1) .. str:sub(from, to):gsub(patt, repl) .. str:sub(to + 1, str:len())
+end
+
 vim.api.nvim_create_hof = function(fun, ...)
   local params = { ... }
   return function()
