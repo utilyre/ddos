a8a620ea52cafc21e49fe18c91a5cc352ee61364
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-04 13:24:23 +0430

    nvim: use 1 signcolumn

--- nvim/.config/nvim/lua/after/general/options.lua
+++ nvim/.config/nvim/lua/after/general/options.lua
@@ -15,7 +15,7 @@ vim.opt.splitbelow = true
 vim.opt.scrolloff = 2
 vim.opt.sidescrolloff = 2
 
-vim.opt.signcolumn = "yes:2"
+vim.opt.signcolumn = "yes"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
