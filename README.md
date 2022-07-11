df190252f72b4514fb4e1f674be1475f4e789c6d
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-11 23:16:02 +0430

    fix installation process

--- README.md
+++ README.md
@@ -4,8 +4,21 @@
 
 ## Installation
 
-Run the install script:
+Make sure you have **git** installed:
 
 ```bash
-curl "https://raw.githubusercontent.com/utilyre/dotfiles/main/install.sh" | sh
+sudo pacman -S git
+```
+
+Clone the repository:
+
+```bash
+git clone https://github.com/utilyre/dotfiles.git
+cd dotfiles
+```
+
+Run the installation script:
+
+```bash
+DOTFILES_CACHE="${XDG_CACHE_HOME:-"$HOME/.cache"}/dotfiles" ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 out() {
-	printf -- "\e[1;32m%s\e[m \e[1m%s\e[m\n" "===>" "$1" # bold
+	printf -- "\e[1;32m%s\e[m \e[1m%s\e[m\n" "===>" "$1"
 }
 
 err() {
@@ -10,28 +10,26 @@ err() {
 }
 
 install_yay() {
-	git clone --depth="1" -- "https://aur.archlinux.org/yay-bin.git"
-	(cd -- "yay-bin" && makepkg --noconfirm --syncdeps --rmdeps --install)
+	git clone --depth="1" -- "https://aur.archlinux.org/yay-bin.git" "$DOTFILES_CACHE/yay-bin"
+	(cd -- "$DOTFILES_CACHE/yay-bin" && makepkg --noconfirm --syncdeps --rmdeps --install)
 }
 
 install_deps() {
 	yay --noconfirm --sync --refresh --sysupgrade
 	yay --noconfirm --sync --removemake - < "package.txt"
-	yay --noconfirm --sync --clean
-	sudo usermod --shell="/bin/zsh" -- "$USER"
 }
 
-download_repo() {
-	git clone --depth="1" -- "https://github.com/utilyre/dotfiles.git"
-	(cd -- "dotfiles" && stow --target="$HOME" */)
+exec_hooks() {
+	sudo usermod --shell="/bin/zsh" -- "$USER"
+	stow --target="$HOME" */
 }
 
 main() {
 	install_yay || err "Failed to install yay. Exiting..."
 	install_deps || err "Failed to install dependencies. Exiting..."
-	download_repo || err "Failed to download repository. Exiting..."
+	exec_hooks || err "Failed to execute hooks. Exiting..."
 
-	out "Installed successfully. Reboot and login to X11 session!"
+	out "Installed successfully. Reboot and login to X session!"
 }
 
 main
