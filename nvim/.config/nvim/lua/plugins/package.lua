b48ac25ee37bbee45500b77526248db70c7f35f8
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-27 10:13:50 +0430

    nvim: change mason keymap

--- nvim/.config/nvim/lua/plugins/package.lua
+++ nvim/.config/nvim/lua/plugins/package.lua
@@ -2,32 +2,31 @@ local packer = require("packer")
 local util = require("packer.util")
 local mason = require("mason")
 
-packer.init({
-  display = {
-    open_fn = vim.get_hof(util.float, { border = "rounded" }),
-  },
-})
-
 packer.startup({
-  function(use)
-    for i, plugin in ipairs(_G.plugins) do
-      use(plugin)
-    end
-  end,
+	function(use)
+		for i, plugin in ipairs(_G.plugins) do
+			use(plugin)
+		end
+	end,
+	config = {
+		display = {
+			open_fn = vim.get_hof(util.float, { border = "rounded" }),
+		},
+	},
 })
 
 mason.setup({
-  ui = {
-    border = "rounded",
-    icons = {
-     package_pending = _G.icons.ui.Pending,
-     package_uninstalled = _G.icons.ui.Unchecked,
-     package_installed = _G.icons.ui.Checked,
-    },
-  },
+	ui = {
+		border = "rounded",
+		icons = {
+			package_pending = _G.icons.ui.Pending,
+			package_uninstalled = _G.icons.ui.Unchecked,
+			package_installed = _G.icons.ui.Checked,
+		},
+	},
 })
 
 vim.keymap.set("n", "<leader>pi", vim.get_hof(packer.install))
 vim.keymap.set("n", "<leader>pu", vim.get_hof(packer.update))
 vim.keymap.set("n", "<leader>pc", vim.get_hof(packer.clean))
-vim.keymap.set("n", "<leader>pt", "<cmd>Mason<cr>")
+vim.keymap.set("n", "<leader>pm", "<cmd>Mason<cr>")
