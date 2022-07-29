97be2466707f06c41dbbb56868829f9fa20a37e6
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-29 23:04:35 +0430

    nvim: update

--- nvim/.config/nvim/lua/plugins/package.lua
+++ nvim/.config/nvim/lua/plugins/package.lua
@@ -4,16 +4,16 @@ local mason = require("mason")
 local ui = require("mason.ui")
 
 packer.startup({
-	function(use)
-		for i, plugin in ipairs(_G.plugins) do
-			use(plugin)
-		end
-	end,
-	config = {
-		display = {
-			open_fn = vim.get_hof(util.float, { border = "rounded" }),
-		},
-	},
+  function(use)
+    for i, plugin in ipairs(_G.plugins) do
+      use(plugin)
+    end
+  end,
+  config = {
+    display = {
+      open_fn = vim.get_hof(util.float, { border = "rounded" }),
+    },
+  },
 })
 
 vim.keymap.set("n", "<leader>pi", vim.get_hof(packer.install))
@@ -21,14 +21,14 @@ vim.keymap.set("n", "<leader>pu", vim.get_hof(packer.update))
 vim.keymap.set("n", "<leader>pc", vim.get_hof(packer.clean))
 
 mason.setup({
-	ui = {
-		border = "rounded",
-		icons = {
-			package_pending = _G.icons.ui.Pending,
-			package_uninstalled = _G.icons.ui.Unchecked,
-			package_installed = _G.icons.ui.Checked,
-		},
-	},
+  ui = {
+    border = "rounded",
+    icons = {
+      package_pending = _G.icons.ui.Pending,
+      package_uninstalled = _G.icons.ui.Unchecked,
+      package_installed = _G.icons.ui.Checked,
+    },
+  },
 })
 
 vim.keymap.set("n", "<leader>m", vim.get_hof(ui.open))
