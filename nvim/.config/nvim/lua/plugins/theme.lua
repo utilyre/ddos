f229816d94d3570ac46220b8a312134755b9411b
Amirabbas Fazelinia <utilyre@proton.me>
2022-08-08 17:39:56 +0430

    nvim: tokyonight theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,62 +1,8 @@
 local devicons = require("nvim-web-devicons")
-local onedark = require("onedark")
+local tokyonight = require("tokyonight")
 
 devicons.setup({
   default = true,
 })
 
-onedark.setup({
-  style = "dark",
-  toggle_style_key = "<nop>",
-  code_style = {
-    comments = "italic",
-    keywords = "italic",
-  },
-  highlights = {
-    NotifyERRORBorder = { fg = "$red" },
-    NotifyERRORIcon = { fg = "$red" },
-    NotifyERRORTitle = { fg = "$red" },
-    NotifyWARNBorder = { fg = "$yellow" },
-    NotifyWARNIcon = { fg = "$yellow" },
-    NotifyWARNTitle = { fg = "$yellow" },
-    NotifyINFOBorder = { fg = "$cyan" },
-    NotifyINFOIcon = { fg = "$cyan" },
-    NotifyINFOTitle = { fg = "$cyan" },
-
-    NavicText = { fg = "$fg" },
-    NavicSeparator = { fg = "$light_grey" },
-    NavicIconsFile = { fg = "$blue" },
-    NavicIconsPackage = { fg = "$orange" },
-    NavicIconsModule = { fg = "$orange" },
-    NavicIconsNamespace = { fg = "$orange" },
-    NavicIconsClass = { fg = "$yellow" },
-    NavicIconsConstructor = { fg = "$blue" },
-    NavicIconsField = { fg = "$purple" },
-    NavicIconsProperty = { fg = "$cyan" },
-    NavicIconsMethod = { fg = "$blue" },
-    NavicIconsStruct = { fg = "$purple" },
-    NavicIconsEvent = { fg = "$yellow" },
-    NavicIconsInterface = { fg = "$green" },
-    NavicIconsEnum = { fg = "$purple" },
-    NavicIconsEnumMember = { fg = "$yellow" },
-    NavicIconsConstant = { fg = "$orange" },
-    NavicIconsFunction = { fg = "$blue" },
-    NavicIconsTypeParameter = { fg = "$red" },
-    NavicIconsVariable = { fg = "$purple" },
-    NavicIconsOperator = { fg = "$red" },
-    NavicIconsNull = { fg = "$orange" },
-    NavicIconsBoolean = { fg = "$orange" },
-    NavicIconsNumber = { fg = "$orange" },
-    NavicIconsString = { fg = "$orange" },
-    NavicIconsKey = { fg = "$orange" },
-    NavicIconsArray = { fg = "$orange" },
-    NavicIconsObject = { fg = "$orange" },
-
-    IndentBlanklineContextChar = { fmt = "nocombine", fg = "$fg" },
-    CommentTSNote = { fmt = "bold", fg = "$blue" },
-    CommentTSWarning = { fmt = "bold", fg = "$yellow" },
-    CommentTSDanger = { fmt = "bold", fg = "$red" },
-  },
-})
-
-onedark.load()
+tokyonight.colorscheme()
