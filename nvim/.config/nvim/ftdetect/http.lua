21242166303d86739c0c6369468c22c427cd78e1
Amirabbas Fazelinia <utilyre@proton.me>
2022-08-29 12:48:31 +0430

    nvim: remoooe http ftdetect

--- nvim/.config/nvim/ftdetect/http.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_augroup("fthttp", {})
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  group = "fthttp",
-  pattern = "*.http",
-  callback = function(a)
-    vim.bo[a.buf].filetype = "http"
-    vim.bo[a.buf].commentstring = "#%s"
-  end,
-})
