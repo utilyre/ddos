771aca81d11bbe305070634d3e5ab19892141aad
Amirabbas Fazelinia <utilyre@proton.me>
2022-09-17 20:46:04 +0430

    nvim: change component sep

--- nvim/.config/nvim/lua/general/icons.lua
+++ nvim/.config/nvim/lua/general/icons.lua
@@ -8,7 +8,7 @@ _G.icons = {
     TruncRight = "",
     Collapsed = "",
     Expanded = "",
-    ComponentSeparator = "·",
+    ComponentSeparator = "│",
     SectionLeft = "",
     SectionRight = "",
   },
