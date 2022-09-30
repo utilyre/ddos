aab8bf9f3f8df3e5a3692fabc1ee6b20866765f9
Amirabbas Fazelinia <utilyre@proton.me>
2022-09-30 18:11:28 +0330

    nvim: enable navic integration

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -8,10 +8,17 @@ devicons.setup({
   default = true,
 })
 
-catppuccin.setup()
+catppuccin.setup({
+  integrations = {
+    navic = {
+      enabled = true,
+      custom_bg = "none",
+    },
+  },
+})
 catppuccin.load()
 
-vim.api.nvim_set_hl(0, "BufferMod", { bg = palette.none, fg = palette.peach })
-vim.api.nvim_set_hl(0, "FidgetTitle", { bold = true, bg = palette.none, fg = palette.yellow })
-vim.api.nvim_set_hl(0, "FidgetTask", { bg = palette.none, fg = palette.subtext0 })
-vim.api.nvim_set_hl(0, "StatusLine", { bg = palette.none, fg = palette.text })
+vim.api.nvim_set_hl(0, "BufferMod", { bg = "none", fg = palette.peach })
+vim.api.nvim_set_hl(0, "FidgetTitle", { bold = true, bg = "none", fg = palette.yellow })
+vim.api.nvim_set_hl(0, "FidgetTask", { bg = "none", fg = palette.subtext0 })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "none", fg = palette.text })
