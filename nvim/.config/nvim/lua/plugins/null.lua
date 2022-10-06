3b9ca395112e8ae63e52149c00b718f53dd3a2d0
Amirabbas Fazelinia <utilyre@proton.me>
2022-10-06 15:11:35 +0330

    nvim: update null-ls

--- nvim/.config/nvim/lua/plugins/null.lua
+++ nvim/.config/nvim/lua/plugins/null.lua
@@ -1,19 +1,16 @@
 local null = require("null-ls")
 
-local get_sources = function()
-  local config = vim.json.decode(vim.fs.read(os.getenv("MASON_CONFIG")) or "{}")
-  local sources = {}
+local sources = {}
 
-  for linter, options in pairs(config.linters or {}) do
-    table.insert(sources, null.builtins.diagnostics[linter].with(options))
-  end
-  for formatter, options in pairs(config.formatters or {}) do
-    table.insert(sources, null.builtins.formatting[formatter].with(options))
-  end
-
-  return sources
+local config = vim.json.decode(vim.fs.read(os.getenv("MASON_CONFIG")) or "{}")
+for linter, options in pairs(config.linters or {}) do
+  table.insert(sources, null.builtins.diagnostics[linter].with(options))
+end
+for formatter, options in pairs(config.formatters or {}) do
+  table.insert(sources, null.builtins.formatting[formatter].with(options))
 end
 
 null.setup({
-  sources = get_sources(),
+  update_in_insert = true,
+  sources = sources,
 })
