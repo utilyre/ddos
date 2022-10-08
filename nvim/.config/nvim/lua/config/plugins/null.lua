6ebb5f3287c7257386d1439e1056890a8ab7b356
Amirabbas Fazelinia <utilyre@proton.me>
2022-10-08 17:56:33 +0330

    nvim: use table.map for null-ls

--- nvim/.config/nvim/lua/config/plugins/null.lua
+++ nvim/.config/nvim/lua/config/plugins/null.lua
@@ -1,15 +1,14 @@
 local null = require("null-ls")
 
-local sources = {}
-
 local config = vim.json.decode(vim.fs.read(os.getenv("MASON_CONFIG")) or "{}")
-for linter, options in pairs(config.linters or {}) do
-  table.insert(sources, null.builtins.diagnostics[linter].with(options))
-end
-for formatter, options in pairs(config.formatters or {}) do
-  table.insert(sources, null.builtins.formatting[formatter].with(options))
-end
 
 null.setup({
-  sources = sources,
+  sources = {
+    unpack(table.map(config.linters or {}, function(options, linter)
+      return null.builtins.diagnostics[linter].with(options)
+    end)),
+    unpack(table.map(config.formatters or {}, function(options, formatter)
+      return null.builtins.formatting[formatter].with(options)
+    end)),
+  },
 })
