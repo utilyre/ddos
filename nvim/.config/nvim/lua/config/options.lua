7dc9f97fc064025da31224544ae6db8b24d07c93
Amirabbas Fazelinia <utilyre@proton.me>
2022-10-12 20:54:11 +0330

    nvim: better options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -16,11 +16,19 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.list = true
-vim.opt.listchars:append("tab:  " .. vim.g.icons.ui.Tab .. ",trail:" .. vim.g.icons.ui.Space)
-vim.opt.fillchars:append("eob: ")
+vim.opt.listchars = {
+  tab = "  " .. vim.g.icons.ui.Tab,
+  trail = vim.g.icons.ui.Space,
+}
+vim.opt.fillchars = {
+  eob = " ",
+}
 
-vim.opt.showmode = false
-vim.opt.inccommand = "split"
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -28,13 +36,11 @@ vim.opt.smartcase = true
 vim.opt.pumheight = 16
 vim.opt.completeopt = {}
 
-vim.opt.wrap = false
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-
 vim.opt.spell = true
-vim.opt.spelloptions:append("camel")
+vim.opt.spelloptions = {
+  "camel",
+  "noplainbuffer",
+}
 
 vim.api.nvim_create_augroup("options", {})
 vim.api.nvim_create_autocmd("InsertLeave", {
@@ -47,6 +53,12 @@ vim.api.nvim_create_autocmd("InsertEnter", {
   group = "options",
   callback = function()
     vim.opt.timeout = true
+  end,
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = "options",
+  callback = function()
+    vim.opt.formatoptions = {}
   end,
 })
 vim.api.nvim_create_autocmd("TextYankPost", {
