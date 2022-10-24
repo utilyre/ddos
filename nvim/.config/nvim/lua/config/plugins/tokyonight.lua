f542d43846f4ec3cbb317274855d7acc1dd7a46d
Amirabbas Fazelinia <utilyre@proton.me>
2022-10-24 23:12:24 +0330

    nvim: rm navic overrides

--- nvim/.config/nvim/lua/config/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/config/plugins/tokyonight.lua
@@ -14,29 +14,6 @@ tokyonight.setup({
     highlights.TelescopeSelectionCaret = { background = colors.bg_visual, foreground = colors.teal }
     highlights.TelescopeMultiIcon = { foreground = colors.red }
     highlights.TelescopeMultiSelection = { bold = true, italic = true }
-
-    highlights.BarbecueMod = highlights.BufferVisibleMod
-    highlights.NavicText = highlights.Normal
-    highlights.NavicSeparator = highlights.Conceal
-    highlights.NavicIconsFile = highlights.CmpItemKindFile
-    highlights.NavicIconsPackage = highlights.CmpItemKindFolder
-    highlights.NavicIconsModule = highlights.CmpItemKindModule
-    highlights.NavicIconsNamespace = highlights.CmpItemKindModule
-    highlights.NavicIconsClass = highlights.CmpItemKindClass
-    highlights.NavicIconsConstructor = highlights.CmpItemKindConstructor
-    highlights.NavicIconsField = highlights.CmpItemKindField
-    highlights.NavicIconsProperty = highlights.CmpItemKindProperty
-    highlights.NavicIconsMethod = highlights.CmpItemKindMethod
-    highlights.NavicIconsStruct = highlights.CmpItemKindStruct
-    highlights.NavicIconsEvent = highlights.CmpItemKindEvent
-    highlights.NavicIconsInterface = highlights.CmpItemKindInterface
-    highlights.NavicIconsEnum = highlights.CmpItemKindEnum
-    highlights.NavicIconsEnumMember = highlights.CmpItemKindEnumMember
-    highlights.NavicIconsConstant = highlights.CmpItemKindConstant
-    highlights.NavicIconsFunction = highlights.CmpItemKindFunction
-    highlights.NavicIconsTypeParameter = highlights.CmpItemKindTypeParameter
-    highlights.NavicIconsVariable = highlights.CmpItemKindVariable
-    highlights.NavicIconsOperator = highlights.CmpItemKindOperator
   end,
 })
 tokyonight.colorscheme()
