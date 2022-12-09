a26d1f46bc48dba131cbe008c23ee23589ce4ff2
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-09 15:34:24 +0330

    nvim: remove background_colour option from notify

--- nvim/.config/nvim/lua/config/plugins/notify.lua
+++ nvim/.config/nvim/lua/config/plugins/notify.lua
@@ -3,7 +3,6 @@ local notify = require("notify")
 notify.setup({
   stages = "slide",
   top_down = false,
-  background_colour = "NormalFloat",
   icons = {
     INFO = vim.g.icons.diagnostic.Information,
     WARN = vim.g.icons.diagnostic.Warning,
