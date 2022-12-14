11155e42e9f59709898a9a6ac48e13fda4991170
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-15 00:40:49 +0330

    nvim: change lines binding to <leader>dl

--- nvim/.config/nvim/lua/config/plugins/diagnostic.lua
+++ nvim/.config/nvim/lua/config/plugins/diagnostic.lua
@@ -39,7 +39,7 @@ vim.fn.sign_define("DiagnosticSignError", {
   text = vim.g.icons.diagnostic.Error,
 })
 
-vim.keymap.set("n", "<leader>dd", vim.callback(lines.toggle))
 vim.keymap.set("n", "<leader>dh", vim.callback(vim.diagnostic.open_float))
+vim.keymap.set("n", "<leader>dl", vim.callback(lines.toggle))
 vim.keymap.set("n", "<leader>dk", vim.callback(vim.diagnostic.goto_prev, { float = false }))
 vim.keymap.set("n", "<leader>dj", vim.callback(vim.diagnostic.goto_next, { float = false }))
