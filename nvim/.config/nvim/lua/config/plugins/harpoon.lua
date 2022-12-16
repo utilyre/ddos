be8d71e6b886b46f6161782824ea6d680716f104
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-16 21:01:54 +0330

    nvim: add keymaps for harpoon

--- nvim/.config/nvim/lua/config/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/config/plugins/harpoon.lua
@@ -8,4 +8,8 @@ harpoon.setup({
 })
 
 vim.keymap.set("n", "<tab>", vim.callback(ui.toggle_quick_menu))
+vim.keymap.set("n", "<c-1>", vim.callback(ui.nav_file, 1))
+vim.keymap.set("n", "<c-2>", vim.callback(ui.nav_file, 2))
+vim.keymap.set("n", "<c-3>", vim.callback(ui.nav_file, 3))
+vim.keymap.set("n", "<c-4>", vim.callback(ui.nav_file, 4))
 vim.keymap.set("n", "<c-m>", vim.callback(mark.add_file))
