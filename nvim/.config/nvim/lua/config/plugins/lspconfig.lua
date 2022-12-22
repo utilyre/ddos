b494473c7055da9ccbc61f0cea56cf8a0c82b51d
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-22 22:38:41 +0330

    nvim: merge inlayhints.lua into lspconfig.lua

--- nvim/.config/nvim/lua/config/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/config/plugins/lspconfig.lua
@@ -1,12 +1,13 @@
 local spec = {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "williamboman/mason.nvim",
+    "lvimuser/lsp-inlayhints.nvim",
   },
 }
 
 function spec.config()
   local lspconfig = require("lspconfig")
+  local inlayhints = require("lsp-inlayhints")
   local cmp = require("cmp_nvim_lsp")
 
   for server, options in pairs(vim.g.servers or {}) do
@@ -14,12 +15,15 @@ function spec.config()
     lspconfig[server].setup(options)
   end
 
+  inlayhints.setup()
+
   vim.api.nvim_create_augroup("lsp", {})
   vim.api.nvim_create_autocmd("LspAttach", {
     group = "lsp",
     callback = function(a)
       local client = vim.lsp.get_client_by_id(a.data.client_id)
 
+      if client.server_capabilities["inlayHintProvider"] then inlayhints.on_attach(client, a.buf) end
       if client.server_capabilities["codeLensProvider"] then
         vim.api.nvim_create_augroup("codelens", { clear = false })
         vim.api.nvim_create_autocmd({ "CursorHold", "TextChanged", "InsertLeave" }, {
