9d3fdc6498f406f1fed78ad6cc31cd19af9767b9
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-22 13:34:58 +0330

    nvim: switch from packer to lazy

--- nvim/.config/nvim/lua/config/plugins/init.lua
+++ nvim/.config/nvim/lua/config/plugins/init.lua
@@ -1,108 +1,179 @@
-local packer_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if not vim.loop.fs_access(packer_path, "R") then
-  os.execute("git clone --depth=\"1\" -- \"https://github.com/wbthomason/packer.nvim.git\" \"" .. packer_path .. "\"")
-  vim.cmd.packadd("packer.nvim")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_access(lazypath, "R") then
+  os.execute(
+    "git clone --single-branch --filter=\"blob:none\" -- \"https://github.com/folke/lazy.nvim.git\" \""
+      .. lazypath
+      .. "\""
+  )
 end
+vim.opt.runtimepath:prepend(lazypath)
 
-local packer = require("packer")
-local util = require("packer.util")
+local lazy = require("lazy")
 
-packer.startup({
-  function(use)
-    use({
-      "wbthomason/packer.nvim",
-      requires = {
-        "nvim-lua/plenary.nvim",
-        "muniftanjim/nui.nvim",
+lazy.setup({
+  { "nvim-lua/plenary.nvim" },
+  { "muniftanjim/nui.nvim" },
 
-        "kyazdani42/nvim-web-devicons",
-        "folke/tokyonight.nvim",
+  {
+    "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("config.plugins.devicons")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("config.plugins.tokyonight")
+    end,
+  },
 
-        "rcarriga/nvim-notify",
-        "folke/noice.nvim",
-
-        "numtostr/fterm.nvim",
-        "kyazdani42/nvim-tree.lua",
-        "nvim-telescope/telescope.nvim",
-        "theprimeagen/harpoon",
-
-        "smiteshp/nvim-navic",
-        "utilyre/barbecue.nvim",
-        "lewis6991/satellite.nvim",
-        "nvim-lualine/lualine.nvim",
-
-        "rrethy/vim-illuminate",
-        "lvimuser/lsp-inlayhints.nvim",
-        "lukas-reineke/indent-blankline.nvim",
-        "lewis6991/gitsigns.nvim",
-        "numtostr/comment.nvim",
-
-        "nvim-treesitter/nvim-treesitter",
-        "p00f/nvim-ts-rainbow",
-
-        "l3mon4d3/luasnip",
-        "rafamadriz/friendly-snippets",
-
-        "williamboman/mason.nvim",
-        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        "neovim/nvim-lspconfig",
-        "jose-elias-alvarez/null-ls.nvim",
-
-        "hrsh7th/nvim-cmp",
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lsp",
-        "petertriho/cmp-git",
-        "hrsh7th/cmp-emoji",
-      },
-      config = function()
-        require("config.plugins.devicons")
-        require("config.plugins.tokyonight")
-
-        require("config.plugins.notify")
-        require("config.plugins.noice")
-
-        require("config.plugins.fterm")
-        require("config.plugins.tree")
-        require("config.plugins.telescope")
-        require("config.plugins.harpoon")
-
-        require("config.plugins.barbecue")
-        require("config.plugins.satellite")
-        require("config.plugins.lualine")
-
-        require("config.plugins.illuminate")
-        require("config.plugins.inlayhints")
-        require("config.plugins.blankline")
-        require("config.plugins.gitsigns")
-        require("config.plugins.comment")
-
-        require("config.plugins.treesitter")
-
-        require("config.plugins.luasnip")
-
-        require("config.plugins.mason")
-        require("config.plugins.diagnostic")
-        require("config.plugins.lsp")
-        require("config.plugins.null")
-
-        require("config.plugins.cmp")
-      end,
-    })
-  end,
-  config = {
-    max_jobs = 4,
-    display = {
-      error_sym = vim.g.icons.package.Pending,
-      working_sym = vim.g.icons.package.Pending,
-      removed_sym = vim.g.icons.package.Uninstalled,
-      done_sym = vim.g.icons.package.Installed,
-      prompt_border = "rounded",
-      open_fn = vim.callback(util.float, { border = "rounded" }),
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      { "rcarriga/nvim-notify" },
     },
+    config = function()
+      require("config.plugins.notify")
+      require("config.plugins.noice")
+    end,
+  },
+
+  {
+    "numtostr/fterm.nvim",
+    config = function()
+      require("config.plugins.fterm")
+    end,
+  },
+  {
+    "kyazdani42/nvim-tree.lua",
+    config = function()
+      require("config.plugins.tree")
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    config = function()
+      require("config.plugins.telescope")
+    end,
+  },
+  {
+    "theprimeagen/harpoon",
+    config = function()
+      require("config.plugins.harpoon")
+    end,
+  },
+
+  {
+    "utilyre/barbecue.nvim",
+    dependencies = {
+      { "smiteshp/nvim-navic" },
+    },
+    config = function()
+      require("config.plugins.barbecue")
+    end,
+  },
+  {
+    "lewis6991/satellite.nvim",
+    config = function()
+      require("config.plugins.satellite")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      { "folke/tokyonight.nvim" },
+    },
+    config = function()
+      require("config.plugins.lualine")
+    end,
+  },
+
+  {
+    "rrethy/vim-illuminate",
+    config = function()
+      require("config.plugins.illuminate")
+    end,
+  },
+  {
+    "lvimuser/lsp-inlayhints.nvim",
+    config = function()
+      require("config.plugins.inlayhints")
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("config.plugins.blankline")
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("config.plugins.gitsigns")
+    end,
+  },
+  {
+    "numtostr/comment.nvim",
+    config = function()
+      require("config.plugins.comment")
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      { "p00f/nvim-ts-rainbow" },
+    },
+    config = function()
+      require("config.plugins.treesitter")
+    end,
+  },
+
+  {
+    "l3mon4d3/luasnip",
+    dependencies = {
+      { "rafamadriz/friendly-snippets" },
+    },
+    config = function()
+      require("config.plugins.luasnip")
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("config.plugins.mason")
+    end,
+  },
+  {
+    url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("config.plugins.diagnostic")
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("config.plugins.lsp")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+      require("config.plugins.null")
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "petertriho/cmp-git" },
+      { "hrsh7th/cmp-emoji" },
+    },
+    config = function()
+      require("config.plugins.cmp")
+    end,
   },
 })
-
-vim.keymap.set("n", "<leader>pc", vim.callback(packer.clean))
-vim.keymap.set("n", "<leader>pi", vim.callback(packer.install))
-vim.keymap.set("n", "<leader>pu", vim.callback(packer.update))
-vim.keymap.set("n", "<leader>ps", vim.callback(packer.sync))
