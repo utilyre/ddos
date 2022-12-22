cb42c333fba38ef0b4e6b653f513bf11796bc0df
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-22 14:25:14 +0330

    nvim: extract lazy to lazy.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,16 +1,5 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_access(lazypath, "R") then
-  os.execute(
-    "git clone --single-branch --filter=\"blob:none\" -- \"https://github.com/folke/lazy.nvim.git\" \""
-      .. lazypath
-      .. "\""
-  )
-end
-vim.opt.runtimepath:prepend(lazypath)
-
-
 require("config.utils")
 require("config.icons")
 require("config.options")
 require("config.keymaps")
-require("lazy").setup("config.plugins")
+require("config.lazy")
---
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,13 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_access(lazypath, "R") then
+  os.execute(
+    "git clone --single-branch --filter=\"blob:none\" -- \"https://github.com/folke/lazy.nvim.git\" \""
+      .. lazypath
+      .. "\""
+  )
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+local lazy = require("lazy")
+
+lazy.setup("config.plugins", {})
