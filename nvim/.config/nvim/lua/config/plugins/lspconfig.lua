665d591b575b8da99932654e0bcb47d5bc7d92de
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-24 09:51:56 +0330

    nvim: allow servers.lua to setup lsp directly

--- nvim/.config/nvim/lua/config/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/config/plugins/lspconfig.lua
@@ -11,10 +11,13 @@ function spec.config()
   local inlayhints = require("lsp-inlayhints")
   local cmp = require("cmp_nvim_lsp")
 
-  local servers_ok, servers = pcall(dofile, vim.fn.stdpath("config") .. "/servers.lua")
-  for server, options in pairs(servers_ok and servers or {}) do
-    options.capabilities = cmp.default_capabilities()
-    lspconfig[server].setup(options)
+  local servers_path = vim.fn.stdpath("config") .. "/servers.lua"
+  if vim.loop.fs_stat(servers_path) then
+    lspconfig.util.on_setup = lspconfig.util.add_hook_after(lspconfig.util.on_setup, function(config)
+      config.capabilities = vim.tbl_deep_extend("force", config.capabilities, cmp.default_capabilities())
+    end)
+
+    dofile(servers_path)
   end
 
   inlayhints.setup()
