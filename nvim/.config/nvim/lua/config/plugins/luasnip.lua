8c894f3543cdb94493f94361b2de35b3efd36d73
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-29 00:26:48 +0330

    nvim: load snippets inside spec.config

--- nvim/.config/nvim/lua/config/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/config/plugins/luasnip.lua
@@ -5,18 +5,15 @@ local spec = {
   },
 }
 
-function spec.init()
-  local vscode = require("luasnip.loaders.from_vscode")
-
-  vscode.lazy_load()
-end
-
 function spec.config()
   local luasnip = require("luasnip")
+  local vscode = require("luasnip.loaders.from_vscode")
 
   luasnip.config.setup({
     region_check_events = "CursorMoved",
   })
+
+  vscode.lazy_load()
 end
 
 return spec
