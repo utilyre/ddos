f6c5419cf417b89f5eb66f16a7bc64ff5be54147
Amirabbas Fazelinia <utilyre@proton.me>
2022-12-30 00:45:10 +0330

    nvim: fmt

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,9 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   os.execute(
-    "git clone --single-branch --filter=\"blob:none\" -- \"https://github.com/folke/lazy.nvim.git\" \""
-      .. lazypath
-      .. "\""
+    "git clone --single-branch --filter=\"blob:none\" -- \"https://github.com/folke/lazy.nvim.git\" \"" .. lazypath .. "\""
   )
 end
 vim.opt.runtimepath:prepend(lazypath)
