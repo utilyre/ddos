e8147b9b3e6b3c5762c674558a5cc49581e25e54
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-05 00:31:45 +0330

    nvim: do not show nav message git

--- nvim/.config/nvim/lua/config/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/config/plugins/gitsigns.lua
@@ -18,8 +18,8 @@ function spec.config()
       vim.keymap.set("n", "<leader>gr", vim.callback(gitsigns.reset_hunk), { buffer = bufnr })
       vim.keymap.set("n", "<leader>ga", vim.callback(gitsigns.stage_hunk), { buffer = bufnr })
       vim.keymap.set("n", "<leader>gu", vim.callback(gitsigns.undo_stage_hunk), { buffer = bufnr })
-      vim.keymap.set("n", "<leader>gk", vim.callback(gitsigns.prev_hunk), { buffer = bufnr })
-      vim.keymap.set("n", "<leader>gj", vim.callback(gitsigns.next_hunk), { buffer = bufnr })
+      vim.keymap.set("n", "<leader>gk", vim.callback(gitsigns.prev_hunk, { navigation_message = false }), { buffer = bufnr })
+      vim.keymap.set("n", "<leader>gj", vim.callback(gitsigns.next_hunk, { navigation_message = false }), { buffer = bufnr })
     end,
   })
 end
