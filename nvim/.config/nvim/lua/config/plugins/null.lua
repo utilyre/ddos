b5dd120414b750975c7f0fc2a4f1bcfb0aebefda
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-07 16:34:46 +0330

    nvim: register each source directly in sources.lua

--- nvim/.config/nvim/lua/config/plugins/null.lua
+++ nvim/.config/nvim/lua/config/plugins/null.lua
@@ -5,12 +5,10 @@ local spec = {
 function spec.config()
   local null = require("null-ls")
 
-  local sources = {}
   local sources_path = vim.fn.stdpath("config") .. "/user/sources.lua"
-  if vim.loop.fs_stat(sources_path) then sources = dofile(sources_path) end
+  if vim.loop.fs_stat(sources_path) then dofile(sources_path) end
 
   null.setup({
-    sources = sources,
     border = "rounded",
   })
 end
