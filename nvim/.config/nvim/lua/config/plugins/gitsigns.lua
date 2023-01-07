2ade8de9166bbe7920357d9dbf603a1081d591fa
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-07 19:03:18 +0330

    nvim: do not pad gitsigns' virt_text

--- nvim/.config/nvim/lua/config/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/config/plugins/gitsigns.lua
@@ -7,8 +7,8 @@ function spec.config()
 
   gitsigns.setup({
     current_line_blame = true,
-    current_line_blame_formatter = "    <author> <author_time:%R> - [<abbrev_sha>] <summary>",
-    current_line_blame_formatter_nc = "    <author>",
+    current_line_blame_formatter = "<author> <author_time:%R> - [<abbrev_sha>] <summary>",
+    current_line_blame_formatter_nc = "<author>",
     current_line_blame_opts = {
       virt_text_priority = 10000,
     },
