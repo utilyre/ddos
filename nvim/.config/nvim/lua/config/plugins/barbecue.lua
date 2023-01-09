16d20033acc6a0fd1a25ba29ae815ef8096e1cc4
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-09 12:02:47 +0330

    nvim: use cursorhold event for bbq for perf

--- nvim/.config/nvim/lua/config/plugins/barbecue.lua
+++ nvim/.config/nvim/lua/config/plugins/barbecue.lua
@@ -8,13 +8,20 @@ local spec = {
 
 function spec.config()
   local barbecue = require("barbecue")
+  local ui = require("barbecue.ui")
 
   barbecue.setup({
+    create_autocmd = false,
     kinds = vim.g.icons.kind,
     symbols = {
       ellipsis = vim.g.icons.layout.Ellipsis,
       separator = vim.g.icons.layout.Chevron,
     },
+  })
+
+  vim.api.nvim_create_autocmd({ "WinScrolled", "BufWinEnter", "CursorHold", "InsertLeave" }, {
+    group = vim.api.nvim_create_augroup("barbecue", {}),
+    callback = vim.callback(ui.update),
   })
 end
 
