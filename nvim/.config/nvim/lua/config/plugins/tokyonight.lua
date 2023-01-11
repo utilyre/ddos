b20c7a6180d08ac53ef6c272c45c5553548b5c44
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-11 23:41:43 +0330

    nvim: better comment highlights

--- nvim/.config/nvim/lua/config/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/config/plugins/tokyonight.lua
@@ -10,10 +10,19 @@ function spec.config()
     style = "night",
     on_colors = function(colors) colors.bg_statusline = colors.none end,
     on_highlights = function(highlights, colors)
-      highlights.TelescopeSelectionCaret =
+      highlights["TelescopeSelectionCaret"] =
         { background = colors.bg_visual, foreground = colors.teal }
-      highlights.TelescopeMultiIcon = { foreground = colors.green }
-      highlights.TelescopeMultiSelection = { bold = true }
+      highlights["TelescopeMultiIcon"] = { foreground = colors.green }
+      highlights["TelescopeMultiSelection"] = { bold = true }
+
+      highlights["@text.note"] =
+        { background = colors.blue2, foreground = colors.bg, bold = true }
+      highlights["@text.todo"] =
+        { background = colors.magenta, foreground = colors.bg, bold = true }
+      highlights["@text.warning"] =
+        { background = colors.yellow, foreground = colors.bg, bold = true }
+      highlights["@text.danger"] =
+        { background = colors.red1, foreground = colors.bg, bold = true }
     end,
   })
 
