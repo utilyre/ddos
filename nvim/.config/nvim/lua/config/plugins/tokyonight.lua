e3b3fa7f9934238d2600a98fe8987828d45a33a9
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-11 23:24:55 +0330

    nvim: uninstall todo-comments

--- nvim/.config/nvim/lua/config/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/config/plugins/tokyonight.lua
@@ -14,13 +14,6 @@ function spec.config()
         { background = colors.bg_visual, foreground = colors.teal }
       highlights.TelescopeMultiIcon = { foreground = colors.green }
       highlights.TelescopeMultiSelection = { bold = true }
-
-      highlights.TodoCommentsNote = { foreground = colors.teal }
-      highlights.TodoCommentsTodo = { foreground = colors.blue2 }
-      highlights.TodoCommentsWarn = { foreground = colors.yellow }
-      highlights.TodoCommentsBug = { foreground = colors.red }
-      highlights.TodoCommentsTest = { foreground = colors.cyan }
-      highlights.TodoCommentsPerf = { foreground = colors.magenta }
     end,
   })
 
