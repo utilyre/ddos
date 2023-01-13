dac1e28333b58d3ca9ea6256114cb84647ed2a32
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-13 22:24:06 +0330

    feat(nvim): implement terminal snapping

--- nvim/.config/nvim/lua/config/plugins/fterm.lua
+++ nvim/.config/nvim/lua/config/plugins/fterm.lua
@@ -1,5 +1,6 @@
 local spec = {
-  "numtostr/fterm.nvim",
+  "numtostr/FTerm.nvim",
+  dev = true,
   dependencies = {
     "samjwill/nvim-unception",
   },
@@ -11,6 +12,49 @@ function spec.config()
   fterm.setup({
     border = "rounded",
     hl = "NormalFloat",
+    on_attach = function(terminal, bufnr)
+      local function snap(direction)
+        local width = 0.8
+        local height = 0.8
+        local x = 0.5
+        local y = 0.5
+        if direction == "left" then
+          width = 0.4
+          x = 0.0
+        elseif direction == "right" then
+          width = 0.4
+          x = 1.0
+        elseif direction == "top" then
+          height = 0.4
+          y = 0.0
+        elseif direction == "bottom" then
+          height = 0.4
+          y = 1.0
+        end
+
+        terminal:setup({
+          dimensions = {
+            width = width,
+            height = height,
+            x = x,
+            y = y,
+          },
+        })
+
+        terminal:close()
+        terminal:open()
+      end
+
+      local function map(left, right)
+        vim.keymap.set("t", "<c-w>" .. left, right, { buffer = bufnr })
+      end
+
+      map("=", vim.callback(snap, "center"))
+      map("H", vim.callback(snap, "left"))
+      map("L", vim.callback(snap, "right"))
+      map("K", vim.callback(snap, "top"))
+      map("J", vim.callback(snap, "bottom"))
+    end,
   })
 
   vim.api.nvim_create_autocmd("User", {
